0103e98777db7812ef129d1f3db58e42f02c7fed
Darragh Enright <darraghenright@gmail.com>
2012-04-28 22:32:47 +0100

    added some test files. added finder tests and initial implementation code. to be refactored. more tests to be added.

--- src/DelimiterFinder.php
+++ src/DelimiterFinder.php
@@ -1,7 +1,36 @@
 <?php
 
+/**
+ * Delimiter Finder
+ *
+ * Attempt to ascertain the delimiter used in a
+ * character separated data file from a defined 
+ * list of likely delimiters.
+ *
+ * @author Darragh Enright <darraghenright@gmail.com>
+ */ 
 class DelimiterFinder
 {
+    /**
+     * @var string $delimiters
+     */
+    private $delimiters = array('\t', ',', ';');
+
+    /**
+     * @var string $file
+     */
+    private $file;
+
+    /**
+     * @var mixed $match
+     */
+    private $match = false;
+    
+    /**
+     * Constructor
+     *
+     * @param string $file Path to the file to read
+     */
     public function __construct($file)
     {
         // TODO: refactor into validator method(s) once unit tested
@@ -23,5 +52,78 @@ class DelimiterFinder
                 'The file %s is empty', $file
             ));
         }
+        
+        $this->file = $file;
+    }
+    
+    /**
+     * Returns the list of currently registered delimiters
+     *
+     * @return array
+     */
+    public function getDelimiters()
+    {
+        return $this->delimiters;
+    }
+    
+    /**
+     * Add a delimiter t the 
+     * @param string
+     */
+    public function addDelimiter($delimiter)
+    {
+        if (1 !== strlen($delimiter)) {
+            throw new UnexpectedValueException(sprintf(
+                'The delimiter "%s" is not a single character', $delimiter
+            ));
+        }
+    
+        if (!in_array($delimiter, $this->delimiters)) {
+            $this->delimiters[] = $delimiter;
+        }
+    }
+    
+    /**
+     * Find!
+     */
+    public function find()
+    {
+        if (false === $this->match) {
+            $this->search();
+        } 
+        
+        return $this->match;
+    }
+        
+    /**
+     * Search
+     */    
+    protected function search()
+    {
+        $handle = fopen($this->file, 'r');
+        $regex  = sprintf('/[^%s]/', implode($this->delimiters));
+        $lines  = array();
+        $loops  = 0;
+        
+        while (!feof($handle)) {
+        
+            $line  = fgets($handle);
+            $chars = preg_replace($regex, null, $line);
+            $count = count_chars($chars, 1);
+        
+            $lines[] = $count;
+            
+            if ($loops++ > 1) {
+                $result = call_user_func_array('array_intersect_assoc', $lines);
+                $k = count($result);
+            
+                if ($k === 1) {
+                    $this->match = chr(key($result));
+                    break;
+                }
+            }
+        }
+        
+        fclose($handle);
     }
 }
--- test/DelimiterFinderTest.php
+++ test/DelimiterFinderTest.php
@@ -4,7 +4,16 @@ require_once __DIR__ . '/../src/DelimiterFinder.php';
 require_once 'PHPUnit/Autoload.php';
 
 class DelimiterFinderTest extends PHPUnit_Framework_TestCase
-{
+{    
+    // find should match delimiter for files.. tab, comma, semicolon etc.
+    // find should match delimiter for custom delim
+    // find should return false for non match... files with no pattern, files with one line etc.
+    // find should throw exception for above if exception flag is set
+    // test with different line endings.... dreaded mac excel CR line endings
+    //    ini_set('auto_detect_line_endings');
+    // replace files with file mocking
+    // use setup and teardown to implement mock files
+    
     /**
      * Set up before tests
      */
@@ -27,7 +36,7 @@ class DelimiterFinderTest extends PHPUnit_Framework_TestCase
      * Omitting a filepath as an argument to 
      * the constructor raises a PHP Error
      *
-     * @group                       Constructor Tests
+     * @group                       Constructor
      * @expectedException           PHPUnit_Framework_Error
      */
     public function testCreateObjectWithMissingArgument()
@@ -39,7 +48,7 @@ class DelimiterFinderTest extends PHPUnit_Framework_TestCase
      * Providing a non-existent filepath as an argument to 
      * the constructor raises an InvalidArgumentException
      *
-     * @group                       Constructor Tests
+     * @group                       Constructor
      * @expectedException           InvalidArgumentException
      * @expectedExceptionMessage    The file files/non_existent_file.csv does not exist
      */
@@ -54,7 +63,7 @@ class DelimiterFinderTest extends PHPUnit_Framework_TestCase
      * Providing a non-readable filepath as an argument to 
      * the constructor throws an InvalidArgumentException
      *
-     * @group                       Constructor Tests
+     * @group                       Constructor
      * @expectedException           InvalidArgumentException
      * @expectedExceptionMessage    The file files/not_readable.csv cannot be read
      */
@@ -69,7 +78,7 @@ class DelimiterFinderTest extends PHPUnit_Framework_TestCase
      * Providing a filepath to an empty file as an argument 
      * to the constructor throws an InvalidArgumentException
      *
-     * @group                       Constructor Tests
+     * @group                       Constructor
      * @expectedException           RuntimeException
      * @expectedExceptionMessage    The file files/empty.csv is empty
      */
@@ -77,15 +86,15 @@ class DelimiterFinderTest extends PHPUnit_Framework_TestCase
     {
         $filepath = 'files/empty.csv';
         $this->assertFileExists($filepath);
-        $this->assertTrue(filesize($filepath) === 0);
+        $this->assertTrue(0 === filesize($filepath));
         $finder = new DelimiterFinder($filepath);
     }
         
     /**
-     * Providing a valid filepath as an argument to 
-     * the constructor creates an instance
+     * Providing a valid filepath as an argument 
+     * to the constructor creates an instance
      *
-     * @group                       Constructor Tests
+     * @group                       Constructor
      */
     public function testObjectCreateWithValidFile()
     {    
@@ -94,4 +103,110 @@ class DelimiterFinderTest extends PHPUnit_Framework_TestCase
         $finder = new DelimiterFinder($filepath);
         $this->assertInstanceOf('DelimiterFinder', $finder);
     }
+
+    /** 
+     * Adding a single character to the list 
+     * of registered delimiters should pass
+     * 
+     * @group                       Add Delimiter
+     */
+    public function testAddDelimiterWithSingleCharacter()
+    {
+        
+        $filepath = 'files/not_empty.csv';
+        $this->assertFileExists($filepath);
+        
+        $delimiter = '|';
+        $finder = new DelimiterFinder($filepath);
+        $finder->addDelimiter($delimiter);
+        $this->assertContains($delimiter, $finder->getDelimiters());
+    }
+    
+    /**
+     * Adding an empty string to the list of registered 
+     * delimiters should throw an UnexpectedValueException
+     * 
+     * @group                       Add Delimiter
+     * @expectedException           UnexpectedValueException
+     * @expectedExceptionMessage    The delimiter "" is not a single character
+     */
+
+    public function testAddDelimiterWithZeroLengthString()
+    {
+        $filepath = 'files/not_empty.csv';
+        $this->assertFileExists($filepath);
+        $finder = new DelimiterFinder($filepath);
+        $finder->addDelimiter(''); 
+    }
+
+    /**
+     * Adding a string longer than one character to the list of 
+     * registered delimiters should throw an UnexpectedValueException
+     *
+     * @group                       Add Delimiter
+     * @expectedException           UnexpectedValueException
+     * @expectedExceptionMessage    The delimiter "--" is not a single character
+     */
+
+    public function testAddDelimiterWithStringsLongerThanOneCharacter()
+    {
+        $filepath = 'files/not_empty.csv';
+        $this->assertFileExists($filepath);
+        $finder = new DelimiterFinder($filepath);
+        $finder->addDelimiter('--'); 
+    }
+    
+    /**
+     * Find comma
+     *
+     * @group                       Find
+     */
+    public function testFinderShouldReturnComma()
+    {
+        $filepath = 'files/delim_comma.csv';
+        $this->assertFileExists($filepath);
+        $finder = new DelimiterFinder($filepath);
+        $this->assertEquals($finder->find(), ',');
+    }
+
+    /**
+     * Find tab
+     *
+     * @group                       Find
+     */
+    
+    public function testFinderShouldReturnTab()
+    {
+        $filepath = 'files/delim_tab.csv';
+        $this->assertFileExists($filepath);
+        $finder = new DelimiterFinder($filepath);
+        $this->assertEquals($finder->find(), "\t");
+    }
+
+    /**
+     * Find semicolon
+     *
+     * @group                       Find
+     */
+    public function testFinderShouldReturnSemicolon()
+    {        
+        $filepath = 'files/delim_semicolon.csv';
+        $this->assertFileExists($filepath);
+        $finder = new DelimiterFinder($filepath);
+        $this->assertEquals($finder->find(), ';');
+    }
+
+    /**
+     * Find custom
+     *
+     * @group                       Find
+     */
+    public function testFinderShouldReturnPipe()
+    {        
+        $filepath = 'files/delim_custom.csv';
+        $this->assertFileExists($filepath);
+        $finder = new DelimiterFinder($filepath);
+        $finder->addDelimiter('|');
+        $this->assertEquals($finder->find(), '|');
+    }    
 }
